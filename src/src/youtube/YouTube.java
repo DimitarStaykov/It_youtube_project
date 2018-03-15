9482eb9f889f57bcf6e5a4a94560a822c8391060
DimitarStaykov <32131915+DimitarStaykov@users.noreply.github.com>
2018-03-15 16:08:48 +0200

    Update YouTube.java
    register, confirm mail, log in in Youtube

--- src/src/youtube/YouTube.java
+++ src/src/youtube/YouTube.java
@@ -20,9 +20,31 @@ public class YouTube {
 		this.videos.remove(video);
 	}
 	
-	public void addUser(User user) {
-		
+	public void register(String name,String password,String password2,String eMail){
+		/*validate name restrictions - existingUsername and etc. 
+		 *validate password
+		 *compare two passwords for quality
+		 * validate eMail format
+		 * If everything passes create user and generate code
+		 * temporary user data is stored in db , inactive users are removed once a day 
+		 * set user as inactive till code is confirmed , has to be a bool that stops logins
+		 * send code to e-mail
+		 */
 	}
+	
+	public void confirmEmail(String s){
+		
+		// if string s matches code in user
+		// change boolean to make account login possible
+	}
+	
+	public void logIn(String name, String password){
+		
+		// search database for input 
+		// generate user page if correct
+		// error page is data not found/incorrect 
+	}
+	
 
 	public boolean existingUserName(String name) {
 		for (User user : users) {
