f43fa3b72369af59d101b210b56337b4b0216ee3
DimitarStaykov <32131915+DimitarStaykov@users.noreply.github.com>
2018-03-15 12:44:50 +0200

    Create Admin.java

---
+++ src/src/youtube/Admin.java
@@ -0,0 +1,13 @@
+package youtube;
+
+public class Admin extends User{
+    
+    boolean isMaster = false;
+    
+    
+    private void removeVideo();
+    
+    private void removeUser();
+    
+    
+}
