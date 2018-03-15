1a62fb94db205dc9150e5169d2f7e0bca79c0bf1
DimitarStaykov <32131915+DimitarStaykov@users.noreply.github.com>
2018-03-15 12:46:26 +0200

    Update Admin.java

--- src/src/youtube/Admin.java
+++ src/src/youtube/Admin.java
@@ -2,6 +2,13 @@ package youtube;
 
 public class Admin extends User{
     
+    // kakwo moje admin-a
+    // da maha videota go ima w user 
+    // da moje da maha user-i
+    // trqbwa li da ima masterAdmin class ? 
+    // admin koito da moje da maha admini ? 
+    
+    
     boolean isMaster = false;
     
     private void removeUser();
