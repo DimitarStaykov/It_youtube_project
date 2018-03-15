2fa76fd37bb93f4b0a61a273d762c474f0eb6f87
DimitarStaykov <32131915+DimitarStaykov@users.noreply.github.com>
2018-03-15 16:30:47 +0200

    Update PlayList.java
    Added date, fixed constructor, added status boolean for private/public and made basic setters.

--- src/src/youtube/PlayList.java
+++ src/src/youtube/PlayList.java
@@ -2,21 +2,25 @@ package youtube;
 
 public class PlayList {
   
-  String name;
-  int numberOfVideos;
-  User user;
-  ArrayList<Video> videos;
+  private String name;
+  private int numberOfVideos;
+  private User user;
+  private ArrayList<Video> videos;
+  private boolean private; //if not private -> public
+  private Date date;
   
   // structurata moje da trqbwa da se promeni
   // trqbwa da mojem kato zadadem da se mahne opredeleno video
   // da maha towa koeto iskame, a ne wsi4ki powtarq6ti se
   // smisal ako ima6 2 edni i sa6ti koeto e wazmojno, da moje6 da mahne6 samo 1
   
-  PlayList(String name,User user){
+  PlayList(String name,User user, boolean status){
       this.name = name;
       this.numberOfVideos = 0;
       this.user = user;
       this.videos = new Arraylist<Video>();
+      this.private = status;
+      this.date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date);
   }
   
   private void changeName(String newName){
@@ -35,5 +39,14 @@ public class PlayList {
     // and remove a specific one 
     // easy if we assume we always know the position of the video in the ArrayList
   }
+  
+  private void setToPublic(){
+    this.private = false;
+  }
+  
+  private void setToPrivate(){
+    this.private = true;
+  }
+  
 
 }
